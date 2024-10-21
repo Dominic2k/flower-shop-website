4b122d92773ab4089663fb26daa270f1b688fbc1
Phạm Đức Đạt <91515103+Dominic2k@users.noreply.github.com>
2024-10-21 09:10:17 +0700

    update home

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 // ------------------------------------------------------------------------------------------------------------------
 // JS Countdown timer
 
-let countdownDate = new Date("Oct 20, 2024 23:59:59").getTime();
+let countdownDate = new Date("Nov 22, 2024 23:59:59").getTime();
 
 let countdownFunction = setInterval(function() {
     let now = new Date().getTime();
@@ -138,7 +138,7 @@ document.getElementById('register-btn').addEventListener('click', function() {
 });
 
 // Đăng nhập người dùng
-let isLogin = false;
+let isLogin = true;
 document.getElementById('login-btn').addEventListener('click', function() {
     const username = document.getElementById('login-username').value;
     const password = document.getElementById('login-password').value;
@@ -158,6 +158,24 @@ document.getElementById('login-btn').addEventListener('click', function() {
     }
 });
 
+
+let dropDownIn = document.querySelector("#openLoginBtn");
+let dropDownUp = document.querySelector("#openRegisterBtn");
+let dropDownOut = document.querySelector("#LogOutBtn");
+
+
+if (isLogin) {
+    dropDownIn.style.display = 'none';
+    dropDownUp.style.display = 'none';
+
+    dropDownOut.style.display = 'block';
+}else {
+    dropDownIn.style.display = 'block';
+    dropDownUp.style.display = 'block';
+
+    dropDownOut.style.display = 'none';
+}
+
 // END MODAL
 // ------------------------------------------------------------------------------------------------------------------
 
@@ -170,167 +188,167 @@ function createdProducts() {
         {
           id: 1,
           title: "Elegant Watch",
-          price: 120.00,
+          price: "1.000.000₫",
           code: "EW2005",
-          originalPrice: 150.00,
+          originalPrice: "1.500.000đ",
           imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Casual",
-          color: "Silver",
+          occasion: "Birthday",
+          color: "Red",
           badgeText: "Best Seller",
           describe: "This elegant silver watch combines style with functionality, making it perfect for everyday casual wear. The sleek design and minimalist face add a touch of sophistication to any outfit. Water-resistant and durable, it's designed for those who value both aesthetics and practicality. A must-have accessory for your wardrobe."
         },
         {
           id: 2,
           title: "Leather Jacket",
-          price: 200.00,
+          price: "300.000₫",
           code:"SQ2005",
-          originalPrice: 250.00,
-          imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Winter",
-          color: "Black",
+          originalPrice: "930.000₫",
+          imageUrl: "https://happyflower.vn/tin-tuc/app/uploads/hoa-hong-mau-vang-1-1.jpg",
+          occasion: "Lovely",
+          color: "Yellow",
           badgeText: "New Arrival",
           describe: "This black leather jacket is a timeless classic, offering both warmth and style during the colder months. Crafted from high-quality leather, it provides excellent insulation while maintaining a sleek, modern look. The jacket features multiple pockets and a comfortable inner lining, making it as practical as it is stylish. Perfect for winter or cool evening outings."
         },
         {
           id: 3,
           title: "Running Shoes",
-          price: 85.00,
+          price: "800.000₫",
           code:"SQ2005",
-          originalPrice: 100.00,
-          imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Sport",
-          color: "Blue",
+          originalPrice: "900.000đ",
+          imageUrl: "https://dienhoatuoi24h.net/wp-content/uploads/2024/01/bo-hoa-baby.jpg",
+          occasion: "Opened",
+          color: "Purple",
           badgeText: "Top Pick",
           describe: "These blue running shoes are designed for athletes seeking comfort and durability. The lightweight construction and breathable fabric keep your feet cool during long runs, while the cushioned sole provides excellent support. Whether you're training for a marathon or enjoying a casual jog, these shoes will help enhance your performance. A great choice for any fitness enthusiast."
         },
         {
           id: 4,
           title: "Sunglasses",
-          price: 50.00,
+          price: "100.000₫",
           code:"SQ2005",
-          originalPrice: 75.00,
+          originalPrice: "200.000đ",
           imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Summer",
-          color: "Black",
+          occasion: "Birthday",
+          color: "Red",
           badgeText: "Limited Edition",
           describe: "These stylish black sunglasses are the perfect accessory for bright summer days. With UV protection, they not only enhance your look but also shield your eyes from harmful sun rays. The lightweight frame ensures all-day comfort, making them ideal for outdoor activities. Elevate your summer style with this limited edition design."
         },
         {
           id: 5,
           title: "Evening Dress",
-          price: 300.00,
+          price: "600.000₫",
           code:"SQ2005",
-          originalPrice: 400.00,
-          imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Formal",
-          color: "Red",
+          originalPrice: "700.000đ",
+          imageUrl: "https://happyflower.vn/tin-tuc/app/uploads/hoa-hong-mau-vang-1-1.jpg",
+          occasion: "Lovely",
+          color: "Yellow",
           badgeText: "Exclusive",
           describe: "This stunning red evening dress is designed to make a statement at any formal event. With its elegant silhouette and rich fabric, it accentuates your curves while maintaining a sophisticated look. The dress is perfect for galas, weddings, or any special occasion where you want to stand out. It’s a timeless piece that will never go out of style."
         },
         {
           id: 6,
           title: "Backpack",
-          price: 75.00,
+          price: "200.000₫",
           code:"SQ2005",
-          originalPrice: 90.00,
-          imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Travel",
-          color: "Gray",
+          originalPrice: "600.000₫",
+          imageUrl: "https://dienhoatuoi24h.net/wp-content/uploads/2024/01/bo-hoa-baby.jpg",
+          occasion: "Opened",
+          color: "Purple",
           badgeText: "Durable",
           describe: "This gray backpack is perfect for travelers and adventurers alike. With its spacious compartments, it can hold everything from laptops to travel essentials, making it ideal for both work and play. The water-resistant material ensures your belongings stay dry, while the padded straps provide all-day comfort. A versatile and durable bag for any journey."
         },
         {
           id: 7,
           title: "Smartphone",
-          price: 999.00,
+          price: "400.000₫",
           code:"SQ2005",
-          originalPrice: 1200.00,
+          originalPrice: "700.000₫",
           imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Everyday",
-          color: "Black",
+          occasion: "Birthday",
+          color: "Red",
           badgeText: "Hot",
           describe: "This latest smartphone is packed with cutting-edge features, including a high-resolution camera, long battery life, and a powerful processor. Its sleek black design is both modern and elegant, making it suitable for both professional and personal use. With ample storage and fast charging capabilities, this phone is perfect for anyone on the go. Stay connected and productive with this must-have gadget."
         },
         {
           id: 8,
           title: "Bluetooth Headphones",
-          price: 150.00,
+          price: "300.000₫",
           code:"SQ2005",
-          originalPrice: 200.00,
-          imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Music",
-          color: "White",
+          originalPrice: "700.000₫",
+          imageUrl: "https://happyflower.vn/tin-tuc/app/uploads/hoa-hong-mau-vang-1-1.jpg",
+          occasion: "Love",
+          color: "Yellow",
           badgeText: "Trending",
           describe: "These wireless Bluetooth headphones provide superior sound quality and noise cancellation, making them ideal for music lovers. The comfortable over-ear design allows for extended listening without discomfort, while the long battery life ensures they’re always ready when you are. Perfect for both commuting and relaxing at home. Enjoy your favorite tunes with exceptional clarity and depth."
         },
         {
           id: 9,
           title: "Coffee Maker",
-          price: 120.00,
+          price: "600.000₫",
           code:"SQ2005",
-          originalPrice: 150.00,
-          imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Kitchen",
-          color: "Black",
+          originalPrice: "700.000₫",
+          imageUrl: "https://dienhoatuoi24h.net/wp-content/uploads/2024/01/bo-hoa-baby.jpg",
+          occasion: "Opened",
+          color: "Purple",
           badgeText: "Best Seller",
           describe: "This automatic coffee maker is a must-have for coffee enthusiasts. It brews fresh, hot coffee in minutes, ensuring you start your day with the perfect cup. With programmable settings and a sleek black design, it fits seamlessly into any kitchen. Whether you prefer a strong espresso or a light brew, this machine delivers consistently great results."
         },
         {
           id: 10,
           title: "Gaming Laptop",
-          price: 1500.00,
+          price: "1.600.000₫",
           code:"SQ2005",
-          originalPrice: 1800.00,
+          originalPrice: "1.800.000đ",
           imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Gaming",
-          color: "Black",
+          occasion: "Birthday",
+          color: "Red",
           badgeText: "High Performance",
           describe: "This high-performance gaming laptop is built for serious gamers. Equipped with a powerful graphics card and fast processor, it handles the most demanding games with ease. The large display offers crisp, vibrant visuals, while the advanced cooling system ensures smooth performance during long sessions. It’s a top choice for those who want to take their gaming experience to the next level."
         },
         {
           id: 11,
           title: "Tennis Racket",
-          price: 200.00,
+          price: "190.000₫",
           code:"SQ2005",
-          originalPrice: 250.00,
-          imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Sport",
-          color: "Green",
+          originalPrice: "250.000đ",
+          imageUrl: "https://happyflower.vn/tin-tuc/app/uploads/hoa-hong-mau-vang-1-1.jpg",
+          occasion: "Lovely",
+          color: "Yellow",
           badgeText: "Professional",
           describe: "This lightweight yet durable tennis racket is designed for both professional players and enthusiasts. With a sturdy frame and comfortable grip, it offers excellent control and power. Whether you're competing in tournaments or practicing your backhand, this racket will help you play your best game. A top-quality choice for any tennis player."
         },
         {
           id: 12,
           title: "Designer Handbag",
-          price: 500.00,
+          price: "600.000₫",
           code:"SQ2005",
-          originalPrice: 600.00,
-          imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Luxury",
-          color: "Pink",
+          originalPrice: "700.000₫",
+          imageUrl: "https://dienhoatuoi24h.net/wp-content/uploads/2024/01/bo-hoa-baby.jpg",
+          occasion: "Opened",
+          color: "Purple",
           badgeText: "Exclusive",
           describe: "This premium designer handbag is the epitome of luxury. Crafted from the finest materials, it features a chic pink design that adds a pop of color to any outfit. Spacious yet stylish, it’s perfect for carrying all your essentials while maintaining an elegant look. A must-have for fashion-forward individuals who appreciate quality and exclusivity."
         },
         {
           id: 13,
           title: "Office Chair",
-          price: 220.00,
+          price: "1.400.000₫",
           code:"SQ2005",
-          originalPrice: 300.00,
+          originalPrice: "1.500.000đ",
           imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Office",
-          color: "Brown",
+          occasion: "Birthday",
+          color: "Red",
           badgeText: "Comfort",
           describe: "This ergonomic office chair is designed to provide maximum comfort during long hours of work. The adjustable height and lumbar support ensure a perfect fit, reducing strain on your back and neck. Its sleek brown design blends seamlessly with any office decor, making it both functional and stylish. Improve your productivity and posture with this essential office chair."
         },
         {
           id: 14,
           title: "Bicycle",
-          price: 800.00,
+          price: "120.000₫",
           code:"SQ2005",
-          originalPrice: 950.00,
-          imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Outdoor",
+          originalPrice: "250.000đ",
+          imageUrl: "https://happyflower.vn/tin-tuc/app/uploads/hoa-hong-mau-vang-1-1.jpg",
+          occasion: "Lovely",
           color: "Yellow",
           badgeText: "Eco-Friendly",
           describe: "This high-quality bicycle is perfect for outdoor enthusiasts looking for an eco-friendly mode of transportation. The lightweight frame and durable tires make it ideal for both city commuting and off-road adventures. With its vibrant yellow design, you’ll stand out while enjoying a smooth, comfortable ride. Get ready to explore the great outdoors with this reliable bike."
@@ -338,11 +356,11 @@ function createdProducts() {
         {
           id: 15,
           title: "Perfume",
-          price: 90.00,
+          price: "800.000₫",
           code:"SQ2005",
-          originalPrice: 120.00,
-          imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Romantic",
+          originalPrice: "920.000đ",
+          imageUrl: "https://dienhoatuoi24h.net/wp-content/uploads/2024/01/bo-hoa-baby.jpg",
+          occasion: "Opened",
           color: "Purple",
           badgeText: "Top Scent",
           describe: "This luxurious perfume features a romantic blend of floral and woody notes, perfect for special occasions. The lightweight frame and durable tires make it ideal for both city commuting and off-road adventures. With its vibrant yellow design, you’ll stand out while enjoying a smooth, comfortable ride. Get ready to explore the great outdoors with this reliable bike."
@@ -350,12 +368,12 @@ function createdProducts() {
         {
           id: 16,
           title: "Electric",
-          price: 130.00,
+          price: "150.000₫",
           code:"SQ2005",
-          originalPrice: 150.00,
+          originalPrice: "170.000đ",
           imageUrl: "https://www.littlereddotflorist.sg/wp-content/uploads/2021/06/Byeol-1.png",
-          occasion: "Romantic",
-          color: "Purple",
+          occasion: "Birthday",
+          color: "Red",
           badgeText: "Top Scent",
           describe: "This luxurious perfume features a romantic blend of floral and woody notes, perfect for special occasions. The lightweight frame and durable tires make it ideal for both city commuting and off-road adventures. With its vibrant yellow design, you’ll stand out while enjoying a smooth, comfortable ride. Get ready to explore the great outdoors with this reliable bike."
         }
@@ -377,7 +395,7 @@ function showListProducts() {
 
     let content = '';
 
-    for (let i = 0; i < storedProducts.length; i++) {
+    for (let i = 0; i < 8; i++) {
         content += `<div class="product-item">`;
         content += `<a href="detail.html?id=` + storedProducts[i].id + `">`;
         content += `<img src="` + storedProducts[i].imageUrl + `" class="card-img" style="height: 400px;">`;
@@ -427,40 +445,43 @@ document.getElementById('feedbackForm').addEventListener('submit', function(even
 
 
 // Tạo 1 feedback mẫu ban đầu và lưu vào local sau đó in ra
-function showListFeedback() {
+function createdFeedbacks() {
+
     const feedbacks  = [
         {
             name: 'Alice Johnson',
             occupation: 'Marketing Manager',
             description: 'The flowers I ordered were absolutely stunning! They\
-                        arrived on time and were fresh. Will definitely be\
-                        ordering again.'
+            arrived on time and were fresh. Will definitely be\
+            ordering again.'
         },
         {
             name: 'John Smith',
             occupation: 'Teacher',
             description: 'I was impressed by the variety of flowers available on\
-                            the website. The bouquet I ordered exceeded my\
-                            expectations.'
+            the website. The bouquet I ordered exceeded my\
+            expectations.'
         },
         {
             name: 'Emily Brown',
             occupation: 'Interior Designer',
             description: 'The customer service was exceptional. They helped me\
-                            choose the perfect arrangement for my friend\'s\
-                            birthday.'
+            choose the perfect arrangement for my friend\'s\
+            birthday.'
         },
         {
             name: 'Michael Lee',
             occupation: 'Software Engineer',
             description: 'I have never been disappointed with the quality of\
-                            flowers from this shop. Highly recommend!'
+            flowers from this shop. Highly recommend!'
         }
     ];
-
-    
     let feedbackJSON = JSON.stringify(feedbacks);
     localStorage.setItem('feedbacks', feedbackJSON);
+};
+
+
+function showListFeedback() {
 
     let storedFeedbacks = JSON.parse(localStorage.getItem('feedbacks'));
 
@@ -523,11 +544,14 @@ cartBtn.addEventListener('click', function() {
         loginModal.style.display = 'block';
     }
 });
+
+
+// Js check login or not logged in
+
+
 // ------------------------------------------------------------------------------------------------------------------
 
 // JS sử lý ô search ở homepage
-
-// JavaScript
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('search-form');
     const searchInput = document.getElementById('input-search');
